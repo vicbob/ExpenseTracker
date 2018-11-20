86ce6fdf9f0962ca207ec6343073cb80e53f17ea
Victor <victor01@shoppi.ng>
2018-11-20 23:46:10 +0100

    added add new expense prompt alert

--- src/pages/expense-groups/expense-groups.ts
+++ src/pages/expense-groups/expense-groups.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
 import { ExpensesPage } from '../expenses/expenses';
+import { AppConstantsProvider } from '../../providers/app-constants/app-constants';
 
 /**
  * Generated class for the ExpenseGroupsPage page.
@@ -16,15 +17,77 @@ import { ExpensesPage } from '../expenses/expenses';
 })
 export class ExpenseGroupsPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+    private alertCtrl: AlertController, private constants:AppConstantsProvider) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ExpenseGroupsPage');
   }
 
-  openPage(num:number,title:string){
-      this.navCtrl.push(ExpensesPage,{'case':num,'title':title});
-    }
+  async addExpense() {
+
   }
+
+  openPage(num: number, title: string) {
+    this.navCtrl.push(ExpensesPage, { 'case': num, 'title': title });
+  }
+
+  presentAddExpensePrompt() {
+    const prompt = this.alertCtrl.create({
+      title: 'Add New Expense',
+      inputs: [
+        {
+          name:"name",
+          placeholder: "name",
+          type: "text"
+        },
+        {
+          name: "price",
+          placeholder: "price",
+          type: "number"
+        },
+        {
+          name: 'category',
+          placeholder: "category",
+          type: "text"
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          handler: data => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Submit',
+          handler: data => {
+            var format = /[ !@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/;
+            let bool: boolean = data.category.match(format) ? true : false;
+            bool = bool || data.category.trim() == "" ? true : false;
+
+            let bool2: boolean = data.price < 5 ? true : false;
+            bool2 = bool2 || data.price.trim() == "" ? true : false
+
+            let bool3:boolean = data.name.match(format)? true:false;
+            bool3 = bool3 || data.name.trim() ==""? true:false;
+
+            if (bool || bool2) {
+              this.constants.presentAlert("Error", "Invalid field");
+              return false;
+            }
+            // try {
+            //   this.editExpense(expense, data.price, data.category);
+            // } catch (error) {
+            //   console.log("Error when calling edit function is ", error);
+            // }
+          }
+        }
+      ]
+    });
+    prompt.present();
+  }
+
+}
 
