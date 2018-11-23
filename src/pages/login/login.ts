d88af2fdc0788cf0b9ffb96a582085f3fef9a244
Victor <victor01@shoppi.ng>
2018-11-23 23:47:22 +0100

    Login form validation

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -3,6 +3,8 @@ import { IonicPage, NavController, NavParams, AlertController, LoadingController
 import { AuthProvider } from '../../providers/auth/auth';
 import { HomePage } from '../home/home';
 import { Storage } from '@ionic/storage';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
+import { UsernameOrEmailValidator } from '../../providers/app-constants/app-constants';
 
 /**
  * Generated class for the LoginPage page.
@@ -22,11 +24,27 @@ export class LoginPage {
   username: string;
   resetPasswordView: boolean = false;
   registerView: boolean = false;
+  loginForm: FormGroup;
 
   constructor(public navCtrl: NavController, public navParams: NavParams,
     private auth: AuthProvider, private alertCtrl: AlertController,
     private storage: Storage, private loadingctrl: LoadingController,
-    public menu: MenuController) {
+    public menu: MenuController, private formBuilder: FormBuilder) {
+    this.loginForm = this.formBuilder.group({
+      usernameOrEmail: ['', Validators.compose([
+        Validators.required,
+        UsernameOrEmailValidator.validUsernameOrEmail,
+        Validators.maxLength(40),
+        Validators.minLength(5),
+      ])
+      ],
+      password: ['', Validators.compose([
+        Validators.required,
+        Validators.maxLength(25),
+        Validators.minLength(5),
+      ])
+      ]
+    });
   }
 
 
@@ -37,12 +55,16 @@ export class LoginPage {
 
 
   async login() {
+    if(!this.loginForm.valid) return false;
     const loader = this.loadingctrl.create({
       content: "Please wait...."
     })
     loader.present();
+    console.log(this.loginForm);
+    let {usernameOrEmail,password} = this.loginForm.value;
+    console.log("Email is ", usernameOrEmail, " and password is ",password);
     try {
-      let resp = await this.auth.login(this.email, this.password)
+      let resp = await this.auth.login(usernameOrEmail, password)
       this.storage.set('token', resp.access_token);
       loader.dismiss();
       this.navCtrl.setRoot(HomePage);
