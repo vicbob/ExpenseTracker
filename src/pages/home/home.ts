35fa2ffa8c6e027693494ee8571a4efd5a214bdc
Victor <victor01@shoppi.ng>
2018-11-25 23:06:03 +0100

    error handling when fetching details

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -5,6 +5,7 @@ import { UserActionsProvider } from '../../providers/user-actions/user-actions';
 import { CategoryGroupsPage } from '../category-groups/category-groups';
 import { ExpenseGroupsPage } from '../expense-groups/expense-groups';
 import { AppConstantsProvider } from '../../providers/app-constants/app-constants';
+import { LoginPage } from '../login/login';
 
 @Component({
   selector: 'page-home',
@@ -23,11 +24,26 @@ export class HomePage {
 
   }
   async ionViewDidLoad(){
+    try{
       await this.userActions.getDetails("Fetching details....");
       this.loader.present();
       this.menu.enable(true);
       this.userDetails = await this.storage.get('user_details');
       this.loader.dismiss();
+    }
+    catch(error){
+      this.loader.dismiss();
+      this.constants.presentToast("An error occured, Please login again",this.logout());
+    }
+
+  }
+
+  async logout(){
+    this.loader.present();
+    await this.userActions.logout();
+    await this.navCtrl.popToRoot();
+    await this.navCtrl.setRoot(LoginPage);
+    this.loader.dismiss();
   }
 
   openPage(char:string){
