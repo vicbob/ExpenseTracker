80e83b1a5505ab63195078c9b5255c733972a183
Victor <victor01@shoppi.ng>
2018-11-25 18:32:22 +0100

    update password and delete account alert

--- src/pages/my-profile/my-profile.ts
+++ src/pages/my-profile/my-profile.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
+import { AppConstantsProvider } from '../../providers/app-constants/app-constants';
 
 /**
  * Generated class for the MyProfilePage page.
@@ -15,11 +16,111 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class MyProfilePage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+    private alertCtrl:AlertController, private constants:AppConstantsProvider) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad MyProfilePage');
   }
 
+  deleteAccount(){
+
+  }
+
+  openPage(page){
+    this.navCtrl.push(page);
+  }
+
+  presentDeleteAlert(){
+    const alert = this.alertCtrl.create({
+      title: "Delete Account?",
+      subTitle: "Are you sure you want to delete your account?",
+      buttons: [{
+        text: "NO"
+      },
+      {
+        text: "YES",
+        handler: () => {
+          this.deleteAccount()
+        }
+      }]
+    });
+    alert.present();
+  }
+
+  presentUpdatePasswordAlert(){
+    const prompt = this.alertCtrl.create({
+      title: 'Update Password',
+      inputs: [
+        {
+          name: "password",
+          placeholder: 'old password',
+          type: "text"
+        },
+        {
+          name: 'newPassword',
+          placeholder: 'new password',
+          type: "text"
+        },
+        {
+          name: 'newPasswordConfirm',
+          placeholder: 'confirm new password',
+          type: "text"
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          handler: data => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Update',
+          handler: data => {
+
+            var format = /[ ]/;
+
+            let bool: boolean = data.password.match(format) ? true : false;
+            bool = bool || data.newPassword.match(format) ? true : false;
+
+            if (bool) {
+              let message:string = "Invalid field, whitespaces not allowed";
+              this.constants.presentAlert("Error", message);
+              return false;
+            }
+
+            bool = data.password.trim()==""?true:false;
+            bool = bool || data.newPassword.trim()==""?true:false;
+
+            if (bool) {
+              let message:string = "Invalid field";
+              this.constants.presentAlert("Error", message);
+              return false;
+            }
+
+            if(data.newPassword != data.newPasswordConfirm){
+              let message:string = "Password mismatch";
+              this.constants.presentAlert("Error", message);
+              return false;
+            }
+
+            try {
+              this.updatePassword(data.password,data.newPassword);
+            } 
+            catch (error) {
+              console.log("Error when calling update password is ", error);
+            }
+          }
+        }
+      ]
+    });
+
+    prompt.present();
+  }
+
+  updatePassword(oldPassword:string,newPassword:string){
+
+  }
 }
